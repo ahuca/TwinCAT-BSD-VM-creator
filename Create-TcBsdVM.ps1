cd6ba73086b983606cbf671a79dde1cc139012f3
Giang Nguyen <giang.nguyen@insta.fi>
2023-05-02 21:19:23 +0300

    Fixed script failure when image path is absolute
    Autoformatted script

--- Create-TcBsdVM.ps1
+++ Create-TcBsdVM.ps1
@@ -1,37 +1,33 @@
 ﻿param(  
-      [Parameter(Mandatory=$true, 
-      HelpMessage="Name your VM")]    
-      $vmname,
-      [Parameter(Mandatory=$false, 
-      HelpMessage="Select your TCBSD image")]    
-      $tcbsdimagefile="TCBSD-x64-12-40411.iso",
-      [Parameter(Mandatory=$false, 
-      HelpMessage="Where is your VirtualBox installation?")]    
-      $virtualBoxPath = 'C:\Program Files\Oracle\VirtualBox'
+    [Parameter(Mandatory = $true, 
+        HelpMessage = "Name your VM")]    
+    $vmname,
+    [Parameter(Mandatory = $false, 
+        HelpMessage = "Select your TCBSD image")]    
+    $tcbsdimagefile = "TCBSD-x64-12-40411.iso",
+    [Parameter(Mandatory = $false, 
+        HelpMessage = "Where is your VirtualBox installation?")]    
+    $virtualBoxPath = 'C:\Program Files\Oracle\VirtualBox'
 )
 
-$workingDirectory=pwd
+$workingDirectory = pwd
 
-if(!(Test-Path $virtualBoxPath))
-{
+if (!(Test-Path $virtualBoxPath)) {
 
     Write-Warning 'You do not have the VirtualBox installed at' $virtualBoxPath  
     $answer = Read-Host 'Would you like me to download the VirtualBox and install it [Y]es [N]o?'
-    if($answer.ToUpperInvariant() -eq 'Y')
-    {
+    if ($answer.ToUpperInvariant() -eq 'Y') {
         Invoke-WebRequest https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe -OutFile VirtualBoxInstall.exe
         .\VirtualBoxInstall.exe    
     }
-    else
-    {
+    else {
         Write-Warning "Installation of VirtualBox not found. Install it manually or if you have it installed on other location than $virtualBoxPath pass the path as parameter of this script and run it again."
         exit
     }
 }
 
 
-if (!(Test-Path $tcbsdimagefile))
-{
+if (!(Test-Path $tcbsdimagefile)) {
     Write-Warning "You will need to login into Beckhoff site to dowload TCBSD image requeste image is: $tcbsdimagefile" 
     Write-Warning "Download the $tcbsdimagefile to $workingDirectory. Then run the script again"
     Write-Warning "Presse any key to go download page"
@@ -40,17 +36,19 @@ if (!(Test-Path $tcbsdimagefile))
     exit   
 }
 
+$tcbsdimagefile = Resolve-Path $tcbsdimagefile
+
 cd $virtualBoxPath
 Write-Output "Creating Virtual Machine TwinCAT 3 BSD"
 
 .\VBoxManage createvm --name $vmname --basefolder $workingDirectory --ostype FreeBSD_64 --register > $null
 .\VBoxManage modifyvm $vmname --memory 1024 --vram 128 --acpi on --hpet on --graphicscontroller vmsvga --firmware efi64 > $null
 
-$installer_image="TcBSD_installer.vdi"
-$runtime_image="TcBSD.vhd"
-$vmdirectory= Join-Path $workingDirectory $vmname
-$sink=Join-Path $vmdirectory $installer_image
-$source=Join-Path $workingDirectory $tcbsdimagefile
+$installer_image = "TcBSD_installer.vdi"
+$runtime_image = "TcBSD.vhd"
+$vmdirectory = Join-Path $workingDirectory $vmname
+$sink = Join-Path $vmdirectory $installer_image
+$source = $tcbsdimagefile
 
 Write-Output "converting img image to virtualbox bootable HDD image"
 $output = .\VBoxManage convertfromraw --format VDI $source $sink 2>&1
@@ -62,7 +60,7 @@ Write-Output "attaching to installation HDD to Sata Port 1"
 .\VBoxManage storageattach $vmname --storagectl "SATA" --device 0 --port 1 --type  hdd --medium $sink > $null
 
 Write-Output "creating empty HDD"
-$runtime_hdd=Join-Path $vmdirectory $runtime_image
+$runtime_hdd = Join-Path $vmdirectory $runtime_image
 $output = .\VBoxManage createmedium --filename $runtime_hdd --size 4096 --format VHD 2>&1
 
 Write-Output "attaching created HDD to Sata Port 0 where we will install TwinCAT BSD"
@@ -76,5 +74,3 @@ Start $vmpath
 Write-Host "Continue the installation of TCBSD in the VirtualBox"
 
 cd $workingDirectory
-
- 
